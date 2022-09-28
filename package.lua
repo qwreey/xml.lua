e286373ffd1c1415f3d0a1614c2f7e4be6169578
console.log(cat == cute) <qwreey75@gmail.com>
2022-09-29 02:34:01 +0900

    Update package.lua

--- package.lua
+++ package.lua
@@ -3,7 +3,7 @@ return {
   version = "2.1.0"
   dependencies = {}
   license = "MIT"
-  homepage = "https://github.com/qwreey75/myXml.lua/new/master"
+  homepage = "https://github.com/qwreey75/myXml.lua"
   description = ""
   tags = {}
 }
