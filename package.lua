b4713072e0c08ba021a815bc549713d5ec04719b
console.log(cat == cute) <qwreey75@gmail.com>
2022-09-29 02:33:25 +0900

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+  name = "luaiod/myXml.lua"
+  version = "2.1.0"
+  dependencies = {}
+  license = "MIT"
+  homepage = "https://github.com/qwreey75/myXml.lua/new/master"
+  description = ""
+  tags = {}
+}
